05a26c33543eca3c68b5d841a1788ce30bd5dc71
Alexandr Kozlenkov <sah4ez32@gmail.com>
2019-03-16 18:56:28 +0300

    Added web page
    Added check HTTP methods. Available GET and POST.
    For GET method added parsing html template with key store.
    For POST method adding new key to store.

--- index.go
+++ index.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 	"sync"
@@ -23,11 +24,35 @@ var (
 	keys = make(map[string]pub)
 )
 
+type PspkStore struct {
+	Title string
+	Keys  map[string]pub
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
 	var (
 		err  error
 		resp = make(map[string]interface{})
 	)
+
+	value := r.Header.Get("X-Access-Token")
+
+	resp["access"] = value == token
+
+	if r.Method == http.MethodGet {
+		lock.RLock()
+		defer lock.RUnlock()
+		tmpl := template.Must(template.ParseFiles("./index.html"))
+		err := tmpl.Execute(w, PspkStore{
+			Title: "PSPK kv store",
+			Keys:  keys,
+		})
+		if err != nil {
+			resp["error"] = err.Error()
+		}
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
 
 	defer func() {
 		if err != nil {
@@ -36,59 +61,57 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(resp)
 	}()
 
-	value := r.Header.Get("X-Access-Token")
+	if r.Method == http.MethodPost {
+		var keyRequest Request
 
-	resp["access"] = value == token
-
-	var keyRequest Request
-
-	err = json.NewDecoder(r.Body).Decode(&keyRequest)
-	if err != nil {
-		return
-	}
-
-	if keyRequest.Key == "" && keyRequest.Name == "" {
-		err = fmt.Errorf("not set values")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	lock.Lock()
-	defer lock.Unlock()
-
-	if keyRequest.Key == "" {
-		key, ok := keys[keyRequest.Name]
-		if ok {
-			resp["key"] = base64.StdEncoding.EncodeToString(key)
-		} else {
-			err = fmt.Errorf("name %s not found", keyRequest.Name)
-			w.WriteHeader(http.StatusNotFound)
+		err = json.NewDecoder(r.Body).Decode(&keyRequest)
+		if err != nil {
 			return
 		}
-	} else {
-		_, ok := keys[keyRequest.Name]
-		if ok {
-			err = fmt.Errorf("name %s is exist", keyRequest.Name)
+
+		if keyRequest.Key == "" && keyRequest.Name == "" {
+			err = fmt.Errorf("not set values")
 			w.WriteHeader(http.StatusBadRequest)
 			return
-		} else {
-			var b pub
-			b, err = base64.StdEncoding.DecodeString(keyRequest.Key)
-			if err != nil {
+		}
+
+		lock.Lock()
+		defer lock.Unlock()
+
+		if keyRequest.Key == "" {
+			key, ok := keys[keyRequest.Name]
+			if ok {
+				resp["key"] = base64.StdEncoding.EncodeToString(key)
+			} else {
+				err = fmt.Errorf("name %s not found", keyRequest.Name)
+				w.WriteHeader(http.StatusNotFound)
 				return
 			}
-
-			if len(b) != 32 {
-				err = fmt.Errorf("should be 32-bytes key")
+		} else {
+			_, ok := keys[keyRequest.Name]
+			if ok {
+				err = fmt.Errorf("name %s is exist", keyRequest.Name)
 				w.WriteHeader(http.StatusBadRequest)
 				return
+			} else {
+				var b pub
+				b, err = base64.StdEncoding.DecodeString(keyRequest.Key)
+				if err != nil {
+					return
+				}
+
+				if len(b) != 32 {
+					err = fmt.Errorf("should be 32-bytes key")
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+
+				keys[keyRequest.Name] = b
+				resp["msg"] = "added"
+
+				w.WriteHeader(http.StatusCreated)
+				return
 			}
-
-			keys[keyRequest.Name] = b
-			resp["msg"] = "added"
-
-			w.WriteHeader(http.StatusCreated)
-			return
 		}
 	}
-
 }
