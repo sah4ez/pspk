c8eff7b1bc63f2c2297ac264d608febb051973a0
Alexandr Kozlenkov <sah4ez32@gmail.com>
2020-05-17 19:50:49 +0300

    Added CORS header

--- handler/index.go
+++ handler/index.go
@@ -84,6 +84,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept")
 
 	if r.Method == "OPTIONS" {
 		return
@@ -171,7 +172,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		}
 		if name := query.Get(pspk.NameSearchKey); name != "" {
 			w.Header().Set("Content-Type", "application/json")
-			keys, err := FindByName(name, r)
+			keys, err := FindByName(name)
 			if err != nil {
 				resp["error"] = err.Error()
 				//todo: fixed handled error
@@ -401,18 +402,13 @@ func GenerateLinkId(data string) (id string, err error) {
 	return link.ID.Hex(), err
 }
 
-func FindByName(name string, r *http.Request) (result []Request, err error) {
-	lastID, limit := loadPagination(r)
+func FindByName(name string) (result []Request, err error) {
 	sess := session.Copy()
 	defer sess.Close()
 
 	c := sess.DB("pspk").C("keys")
-	query := bson.M{"name": bson.RegEx{Pattern: name + ".*"}}
 
-	if lastID != "" {
-		query["_id"] = bson.M{"$gt": lastID}
-	}
-	err = c.Find(query).Limit(limit).Sort("name").All(&result)
+	err = c.Find(bson.M{"name": bson.RegEx{Pattern: name + ".*"}}).Sort("name").All(&result)
 	if err != nil {
 		return
 	}
